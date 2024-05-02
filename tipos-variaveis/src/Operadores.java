1a64c63e89b5be6e96b74c9dda174780fd69781e
bsp486 <bruno486@gmail.com>
2024-05-02 11:56:38 -0300

    trabalhando com operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,23 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int numero = 5;
+        System.out.println(- numero); //-5
+        System.out.println(numero); //5
+        numero = -numero;
+        System.out.println(numero); //-5
+        numero = +numero;
+        System.out.println(numero); //-5
+        numero = numero * -1;
+        System.out.println(numero); //5
+        numero = numero + 1;
+        System.out.println(numero); //6
+        numero++;
+        System.out.println(numero); //7
+        System.out.println(numero++); //7
+        System.out.println(numero); //8
+        System.out.println(++numero);//9
+        System.out.println(numero);//9
+
+
+    }
+}
