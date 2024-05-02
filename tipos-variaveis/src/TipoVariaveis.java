0d58943f3c34b6b789ca4101df2cecae01cf1620
bsp486 <bruno486@gmail.com>
2024-05-02 11:56:55 -0300

    trabalhando com operadores

--- tipos-variaveis/src/TipoVariaveis.java
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -1,5 +1,10 @@
 public class TipoVariaveis {
     public static void main(String[] args) {
+        int idade = 10;
         System.out.println("ola mundo");
+        final double PI = 3.14;
+
+        System.out.println(idade);
+        System.out.println(PI);
     }
 }
